44323f7b0e3647db9756712755386f80f9b316f8
Rácz Gergő <raczgprogramming@gmail.com>
2024-08-11 23:06:25 +0200

    V1.1.0.0-SubV1.1
    Al and Fred added to AI-s.py

--- AI-s.py
+++ AI-s.py
@@ -6,3 +6,8 @@ class AI:
         self.epsilon=epsilon
         self.epsilon_decay=epsilon_decay
         self.epsilon_min=epsilon_min
+
+
+al = AI("Al",0.1,0.9,0.1,0,0.1)
+
+fred = AI("Fred",0.1,0.9,1.0,0.995,0.01)
