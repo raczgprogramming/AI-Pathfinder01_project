81f620b4a158192cf48aee2fbb16c023e91c02b2
Rácz Gergő <raczgprogramming@gmail.com>
2024-08-16 00:19:38 +0200

    V1.1.0.0
    New AI-s added, AI choosing option added, episod limited in 1001 steps, logfile saving bug fixed

--- AIs.py
+++ AIs.py
@@ -7,7 +7,37 @@ class AI:
         self.epsilon_decay=epsilon_decay
         self.epsilon_min=epsilon_min
 
+    def __str__(self):
+        return f"Név: {self.name} (a:{self.alpha}, g:{self.gamma}, e:{self.epsilon}, ed:{self.epsilon_decay}, em:{self.epsilon_min})"
 
-al = AI("Al",0.1,0.9,0.1,1,0.1)
+al = AI("Al", 0.1, 0.9, 0.1, 1, 0.1)
+fred = AI("Fred", 0.1, 0.9, 1.0, 0.995, 0.01)
+nick = AI("Nick", 0.2, 0.9, 0.5, 0.995, 0.0001)
 
-fred = AI("Fred",0.1,0.9,1.0,0.995,0.01)
+ais = [al, fred, nick]
+
+def ai_list():
+    listed_ai = []
+    for i, ai in enumerate(ais, start=1):
+        listed_ai.append(f"{i} - {ai}")
+    return listed_ai
+
+def ai_select():
+    ai_list()
+    try:
+        selection=int(input("Add meg a választott AI számát: "))
+    except ValueError as v:
+        selection=f"Érvénytelen érték!\n{v}"
+    except Exception as e:
+        selection=f"Hiba a futás közben!\n{e}"
+    return selection
+
+def ai_set():
+    select = ai_select()
+    if select is not int:
+        selected_ai = select
+    if select>=1 and select<=len(ais):
+        selected_ai = ais[select - 1]
+    else:
+        selected_ai = "Érvénytelen választás!"
+    return selected_ai
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import numpy as np
 from datetime import datetime
 import AIs
+import os
 
-nameversion = "Pathfinder1_V1.1.0.0-SubV"
+nameversion = "Pathfinder1_V1.1.0.0"
 print(f"Name and version: {nameversion}")
 
 now1 = datetime.now()
-formatter = "%Y%m%d-%H%M%S"
+formatter = "%Y%m%d_%H%M%S"
 nowstrf = now1.strftime(formatter)
+
+print("Választd ki az AI-t az alábbiak közül:")
+ailist = AIs.ai_list()
+for ai in ailist:
+    print(ai)
+selected_ai = AIs.ai_set()
 
 #10x10-es rács definiálása
 grid_size = 10
@@ -19,7 +26,7 @@ grid = np.zeros((grid_size, grid_size))
 path = [(0,0), (0,1), (0,2), (1,2), (2,2), (3,2), (3,3), (3,4), (2,4), (1,4), (1,5), (1,6),
         (2,6), (3,6), (4,6), (5,6), (6,6), (7,6), (8,6), (9,6), (9,7,), (9,8), (9,9)]
 
-#Funkció a szomszédos lépések megtalálására
+#Funkció a szomszédos lépések megtalálására (Jelenleg nem használt)
 def get_neighbors(position):
     neighbors = []
     x, y = position
@@ -33,12 +40,17 @@ def get_neighbors(position):
         neighbors.append((x, y+1))
     return neighbors
 
+def file_location(filename):
+    script_dir = os.path.dirname(__file__)
+    log_file_path = os.path.join(script_dir, filename)
+    return log_file_path
+
 #Q-learning paraméter
-alpha = AIs.al.alpha #tanulási ráta
-gamma = AIs.al.gamma #diszkont faktor
-epsilon = AIs.al.epsilon #kezdeti felfedezés/exploitáció arány
-epsilon_decay = AIs.al.epsilon_decay #epsilon csökkentési arány minden epizód után
-epsilon_min = AIs.al.epsilon_min #minimum epsilon érték
+alpha = selected_ai.alpha #tanulási ráta
+gamma = selected_ai.gamma #diszkont faktor
+epsilon = selected_ai.epsilon #kezdeti felfedezés/exploitáció arány
+epsilon_decay = selected_ai.epsilon_decay #epsilon csökkentési arány minden epizód után
+epsilon_min = selected_ai.epsilon_min #minimum epsilon érték
 
 #Q-tábla inicializálása
 Q = np.zeros((grid_size, grid_size, 4))
@@ -99,7 +111,8 @@ def q_learning():
 
         print(f"Episode {episode} completed in {step_counter} steps with total reward: {total_reward}")
         if episode == 0 or episode % 9 == 0:
-            with open(f'{nameversion}-{nowstrf}-episodes_and_steps_log.txt', "a") as log:
+            ep_and_steps = file_location(f'{nameversion}-{nowstrf}-episodes_and_steps_log.txt')
+            with open(ep_and_steps, "a") as log:
                 rownumber = 0
                 log.write(f'Q-Learning paramether in EP{episode}: Alpha: {alpha}, Gamma: {gamma}, Epsilon: {epsilon}\n')
                 for row in states:
@@ -107,7 +120,8 @@ def q_learning():
                     log.write(f"Step{rownumber}/{step_counter} - {row}, Ep{episode}\n")
                 log.write(f'Episode {episode} completed in {step_counter} steps with total reward: {total_reward}\n\n')
         
-        with open(f'{nameversion}-{nowstrf}-episode_rewards_log.txt', "a") as alog: 
+        ep_rewards = file_location(f'{nameversion}-{nowstrf}-episode_rewards_log.txt')
+        with open(ep_rewards, "a") as alog: 
             alog.write(f'Episode {episode} completed in {step_counter} steps with total reward: {total_reward}\n')
             alog.write(f'Q-Learning paramether: Alpha: {alpha}, Gamma: {gamma}, Epsilon: {epsilon}\n\n')
 
